5d65a2ba22e685a67c7c957064c5b871c6696d96
Gopal dubey <113363461+dubey123459@users.noreply.github.com>
2023-05-24 08:05:21 +0530

    add scroll to top

--- Script.js
+++ Script.js
@@ -18,19 +18,23 @@ window.onscroll = () => {
     // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     //     mybutton.style.display = "block";
     // } else {
-    //     mybutton.style.display = "none";
+    //    
     // }
     //   till
     sections.forEach(sec => {
+        let mybutton = document.getElementById("myBtn");
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        if (top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height && document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
             navlinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                mybutton.style.display = "block";
             });
+        }else{
+            mybutton.style.display = "none";
         }
     })
     // sticky navbar
@@ -64,7 +68,7 @@ ScrollReveal().reveal('.home-content h3,.home-content p, .about-content', { orig
 
 //  scroll to top 
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+// let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 // window.onscroll = function() {scrollFunction()};
@@ -74,10 +78,10 @@ let mybutton = document.getElementById("myBtn");
 // }
 
 // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//     document.body.scrollTop = 0; // For Safari
-//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 // sending email
 function SendEmail() {
     Email.send({
